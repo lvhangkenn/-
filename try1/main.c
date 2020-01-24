1fc916731722dd4c6dedd9a5477857c07fd0c186
lvhang <1666803606@qq.com>
2020-01-24 21:01:47 +0800

    Initial Commit

---
+++ try1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  try1
+//
+//  Created by 吕航 on 2020/1/24.
+//  Copyright © 2020 吕航. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
